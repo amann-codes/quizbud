17ad2f4e52b5c2e9e95fb4d370688a8a05d8fde8
amann-codes <aman.prajapati3000@gmail.com>
2025-10-08 11:24:54 +0530

    feat: server action to create users

---
+++ actions/createUser.ts
@@ -0,0 +1,41 @@
+"use server"
+
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+
+export const createUser = async ({ name, email, password }: { name: string, email: string, password: string }) => {
+    try {
+        if (!email || !password) {
+            return ({
+                status: 400,
+                statusText: 'Email and password are required'
+            })
+        }
+
+        const existingUser = await prisma.user.findFirst({
+            where: {
+                email
+            }
+        })
+
+        if (existingUser) {
+            return ({
+                statusText: "Invalid email address"
+            })
+        }
+        const hashedPassword = await bcrypt.hashSync(password);
+        const user = await prisma.user.create({
+            data: {
+                name, email, password: hashedPassword
+            }
+        })
+        if (!user) {
+            return ({
+                status: 400,
+                statusText: "Failed to create account"
+            })
+        }
+    } catch (error) {
+        throw new Error(`Error occured while creating account: ${error}`)
+    }
+}
