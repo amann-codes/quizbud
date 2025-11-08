aa5717b340990cdbde110aecf4eb11669866a5fc
amann-codes <aman.prajapati3000@gmail.com>
2025-11-08 14:15:25 +0530

    chroe: added get initials function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getInitials = (name: string) => {
+  const parts = name.trim().split(/\s+/);
+  const initials = parts.map(p => p.charAt(0).toUpperCase());
+  return initials.join('');
+}
+
+export function requestFullScreen() {
+  const elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if ((elem as any).webkitRequestFullscreen) {
+    (elem as any).webkitRequestFullscreen();
+  } else if ((elem as any).msRequestFullscreen) {
+    (elem as any).msRequestFullscreen();
+  }
+}
