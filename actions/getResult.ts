37a058c1ca105a790c618ebeb714f45e26cbfa95
amann-codes <aman.prajapati3000@gmail.com>
2025-11-16 16:18:16 +0530

    feat: server action to get test results

---
+++ actions/getResult.ts
@@ -0,0 +1,76 @@
+"use server"
+
+import { getSession } from "./getSession"
+import prisma from "@/lib/prisma";
+
+export async function getResult(testId: string) {
+    try {
+        if (!testId) {
+            throw new Error(`Id is required to get result: ${testId}`)
+        }
+        const session = await getSession();
+        const result = await prisma.test.findUnique({
+            where: {
+                id: testId,
+                AND: [
+                    { userId: session.user.id },
+                    { testStatus: "COMPLETED" }
+                ]
+            },
+            select: {
+                quiz: {
+                    select: {
+                        creator: {
+                            select: {
+                                name: true
+                            }
+                        },
+                        name: true
+                    },
+                },
+                questions: {
+                    select: {
+                        id: true,
+                        explanation: true,
+                        options: {
+                            select: {
+                                id: true,
+                                userSelected: true,
+                                correct: true,
+                                option: true,
+                            }
+                        },
+                        question: true,
+                        skip: true
+                    }
+                }
+            }
+        })
+
+        const correct = result?.questions.filter((q) => q.options.some((o) => o.userSelected && o.correct));
+        const incorrect = result?.questions.filter((q) => q.options.some((o) => o.userSelected && !o.correct))
+        const notAnswered = result?.questions.filter(
+            (q) =>
+                q.skip === false &&
+                !q.options.some(o => o.userSelected === true)
+        )
+        const skipped = result?.questions.filter((q) => q.skip == true);
+        const creator = result?.quiz.creator.name;
+        const quizName = result?.quiz.name;
+        console.log("skipped", skipped)
+        console.log("notAnswered", notAnswered)
+        return {
+            correct: correct ?? [],
+            incorrect: incorrect ?? [],
+            skipped: skipped ?? [],
+            notAnswered: notAnswered ?? [],
+            quizName: quizName ?? "",
+            creator: creator ?? ""
+        }
+
+
+    } catch (e) {
+        console.log(`Error gettin your result: ${e}`)
+        throw new Error(`Error getting your result: ${e}`)
+    }
+}
