39419c9d17aa2f911e79ffe360f0b77ed1fc60fe
amann-codes <aman.prajapati3000@gmail.com>
2025-11-18 23:17:53 +0530

    fix: getting an array of all the events in a time period and processing those events at once

--- actions/updateTest.ts
+++ actions/updateTest.ts
@@ -2,13 +2,14 @@
 
 import { EventPayload } from "@/lib/types";
 
-export async function updateTest({ payload, testId }: { payload: EventPayload, testId: string }) {
+export async function updateTest({ events, testId }: { events: EventPayload[], testId: string }) {
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/test/update/${testId}/`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(payload),
+            body: JSON.stringify(events),
         });
+        console.log(events)
         if (!res.ok) throw new Error("Failed to update test");
         return await res.json();
     } catch (err) {
